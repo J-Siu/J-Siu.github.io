16ddb6d46e6888f27fb17dc3c7468c411c22747e
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2022-08-02 17:17:01 -0400

    update pin_to_me

--- app/pin_to_me/flutter_service_worker.js
+++ app/pin_to_me/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9ac0fea0b9f94bdf84a5a70dcfb9fc56",
-"index.html": "e4b01b912648e5f9d8ef646ea34a4e01",
-"/": "e4b01b912648e5f9d8ef646ea34a4e01",
-"main.dart.js": "5cad0cd89464a004c68121092bc895dc",
+"index.html": "3002b0cd35abc8e81ed1dda84a8deb19",
+"/": "3002b0cd35abc8e81ed1dda84a8deb19",
+"main.dart.js": "71bafd70d565044f653e6f618b34211d",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "b86780667950fad38001b62fc320194c",
 "icons/Icon-192.png": "29b9dbbe1ebce3861577a38ab278bdc8",
